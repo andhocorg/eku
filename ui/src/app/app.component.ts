7b00f0e43f25c36ac96e88f588f4f3e67e5cddf8
Moeenahmad <moeenranjha01@gmail.com>
2022-06-14 22:31:57 +0500

    webpack task is added

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "hellow-app",
+    styles: [ require("./app.component.scss") ],
+    template:require( "./app.component.html"),
 })
-export class AppComponent {
-  title = 'ui';
+export class HelloComponent {
+    message = "Hoo Ah!";
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {HelloComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:      [BrowserModule, FormsModule ], // import Angular's BrowserModule
+    bootstrap:    [HelloComponent],  // indicate the bootstrap component
+    declarations: [HelloComponent] // register our component with the module
 })
-export class AppModule { }
+export class AppModule {}
